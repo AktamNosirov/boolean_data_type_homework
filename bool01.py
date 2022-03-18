b5afdcb30fce7cdeca6c66385a8b976103151d8d
AktamNosirov <94_aktam@mail.ru>
2022-03-18 06:28:04 +0300

    1

--- bool01.py
+++ bool01.py
@@ -1,13 +1,4 @@
 def main(a,b):
-    """
-    Check the logic "they are equal"
-    Args:
-        a: int
-        b: int
-    Returns:
-        bool
-    """
-    # Write your code here
-   return a==b
+    return a==b
 print(main(2,3))
 
